35d0e67b108dcdc6ee0dd66f29d00467cb22cd94
hamed <amzhamed@gmail.com>
2025-05-13 15:52:34 -0700

    Add tail light

--- led.py
+++ led.py
@@ -3,9 +3,10 @@ import board
 import neopixel_spi as neopixel
 
 NUM_PIXELS = 46
-PIXEL_ORDER = neopixel.GRB
+PIXEL_ORDER = neopixel.RGB
 COLORS = (0xFF0000, 0x00FF00, 0x0000FF)
-DELAY = 0.5
+DELAY = 0.1
+LENGTH = 5
 
 spi = board.SPI()
 
@@ -13,12 +14,31 @@ pixels = neopixel.NeoPixel_SPI(
     spi, NUM_PIXELS, pixel_order=PIXEL_ORDER, auto_write=False
 )
 
-# pixels.fill((0,0,0))
 
-while True:
-    for color in COLORS:
-        for i in range(NUM_PIXELS):
-            pixels[i] = color
-            pixels.show()
-            time.sleep(DELAY)
-            pixels.fill(0)
+# pixels.fill((0,0,0))
+def rgb_to_hex_int(r, g, b):
+    return (r << 16) | (g << 8) | b
+
+
+if __name__ == "__main__":
+    base_color = (227, 253, 255)
+    colors = []
+
+    for i in range(LENGTH):
+        brightness = (LENGTH - i) / LENGTH
+        colors.append(rgb_to_hex_int(
+            int(base_color[0] * brightness),
+            int(base_color[1] * brightness),
+            int(base_color[2] * brightness))
+        )
+
+    try:
+        while True:
+            for i in range(NUM_PIXELS):
+                for i in range(LENGTH):
+                    pixels[(i+LENGTH) % NUM_PIXELS] = colors[i]
+                pixels.show()
+                time.sleep(DELAY)
+                pixels.fill(0)
+    finally:
+        pixels.fill((0, 0, 0))
