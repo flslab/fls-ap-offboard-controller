0c6f7baafebbd9e76a949fa37ba63b7bfedb83bb
hamed <amzhamed@gmail.com>
2025-05-13 15:24:10 -0700

    Switch to SPI

--- led.py
+++ led.py
@@ -1,61 +1,24 @@
-#!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
-
 import time
-from rpi_ws281x import *
-import json
-import argparse
+import board
+import neopixel_spi as neopixel
 
-# LED strip configuration:
-LED_COUNT = 46  # Number of LED pixels.
-# LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
-LED_PIN = 10  # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10  # DMA channel to use for generating a signal (try 10)
-LED_BRIGHTNESS = 65  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
+NUM_PIXELS = 46
+PIXEL_ORDER = neopixel.GRB
+COLORS = (0xFF0000, 0x00FF00, 0x0000FF)
+DELAY = 0.5
 
+spi = board.SPI()
 
-class LED:
-    def __init__(self):
-        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS,
-                                       LED_CHANNEL)
-        self.strip.begin()
+pixels = neopixel.NeoPixel_SPI(
+    spi, NUM_PIXELS, pixel_order=PIXEL_ORDER, auto_write=False
+)
 
-    def set_frame(self, brightness_map):
-        for idx, brightness in brightness_map.items():
-            b = brightness_map[idx] / 10
-            self.strip.setPixelColor(int(idx), Color(int(b * 227), int(b * 253), int(b * 255)))
-        self.strip.show()
+# pixels.fill((0,0,0))
 
-    # Define functions which animate LEDs in various ways.
-    def colorWipe(self, color, wait_ms=50):
-        """Wipe color across display a pixel at a time."""
-        for i in range(self.strip.numPixels()):
-            self.strip.setPixelColor(i, color)
-            self.strip.show()
-            time.sleep(wait_ms / 1000.0)
-
-    def clear(self):
-        self.colorWipe(Color(0, 0, 0), 10)
-
-
-if __name__ == '__main__':
-    led = LED()
-
-    led.colorWipe(Color(227, 253, 255))
-
-    # with open("animation_data.json", "r") as f:
-    #     animation_data = json.load(f)
-    #
-    # for i in range(5):
-    #     for frame, data in animation_data.items():
-    #         led.set_frame(data['led'])
-    #         time.sleep(1 / 24)
-
-    led.clear()
+while True:
+    for color in COLORS:
+        for i in range(NUM_PIXELS):
+            pixels[i] = color
+            pixels.show()
+            time.sleep(DELAY)
+            pixels.fill(0)
