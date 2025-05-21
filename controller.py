3eebee4a17cb97d52234838e9b1f5ec719af62f6
hamed <amzhamed@gmail.com>
2025-05-21 14:13:52 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -363,11 +363,11 @@ class Controller:
         self.logger.info("Sending")
         # points = [(0.6, 1.7, 0), (0.35, 2, 0), (0, 1.7, 0), (0.15, 1.2, 0), (0.35, 1, 0),
         #  (0.55, .8, 0), (0.7, 0.3, 0), (0.35, 0, 0), (0.1, 0.3, 0)]
-        points = [(0, 0, 0), (0, 0, 0.1)]
+        points = [(0, 0, 0), (0.2, 0, 0)]
 
         for j in range(10):
             for point in points:
-                for i in range(10):
+                for i in range(20):
                     if self.battery_low:
                         return
                     self.send_position_target(point[0], point[1], -1 - point[2])
