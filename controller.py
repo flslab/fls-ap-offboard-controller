194156a5a21256a534933f89b7fd03d6e5a00a14
hamed <amzhamed@gmail.com>
2025-05-22 15:01:49 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -394,7 +394,7 @@ class Controller:
 
         for j in range(1):
             for point in points:
-                for i in range(25):
+                for i in range(30):
                     if self.battery_low:
                         return
                     self.send_velocity_target(point[0], point[1], point[2])
