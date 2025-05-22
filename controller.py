7c2920f36f758a359133f9dc858d09f642524b8b
hamed <amzhamed@gmail.com>
2025-05-22 14:45:14 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -390,7 +390,7 @@ class Controller:
 
     def test_trajectory(self, x=0, y=0, z=0):
         self.logger.info("Sending")
-        points = [(x, y, z), (0, 0, 0)]
+        points = [(x, y, z), (0, 0, 0), (0, 0, 0)]
 
         for j in range(1):
             for point in points:
