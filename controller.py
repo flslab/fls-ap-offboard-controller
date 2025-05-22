8424717e30eda6e668d041c6505240b62cf76979
hamed <amzhamed@gmail.com>
2025-05-22 14:27:15 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -328,7 +328,8 @@ class Controller:
             int((time.time() - self.start_time) * 1000),  # milliseconds since start
             self.master.target_system,
             self.master.target_component,
-            mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+            # mavutil.mavlink.MAV_FRAME_LOCAL_NED,
+            mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
             0b110111000111,  # only velocity and yaw
             0, 0, 0,
             vx, vy, vz,  # velocity
