5a9571a3cb84956bc03490d62fff67aeaeecde1d
hamed <amzhamed@gmail.com>
2025-05-22 14:38:01 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -444,7 +444,7 @@ class Controller:
         time.sleep(5)
 
         # flight_thread = Thread(target=self.send_trajectory_from_file, args=(args.trajectory,))
-        flight_thread = Thread(target=self.test_trajectory, args=(-2, 0, 0))
+        flight_thread = Thread(target=self.test_trajectory, args=(5, 0, 0))
         # flight_thread = Thread(target=self.circular_trajectory)
 
         battery_thread.start()
