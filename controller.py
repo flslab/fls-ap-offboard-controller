ee88307f2d53c117f773b4d9a27359efa7a7bf65
hamed <amzhamed@gmail.com>
2025-05-28 15:53:37 -0700

    add position estimation

--- controller.py
+++ controller.py
@@ -475,8 +475,8 @@ class Controller:
 
                 self.master.mav.vision_position_estimate_send(
                     usec,  # Timestamp (microseconds)
-                    y,  # X
-                    -x,  # Y
+                    0,  # X y
+                    0,  # Y -x
                     0,  # Z (down is negative)
                     0,  # Roll
                     0,  # Pitch
