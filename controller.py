1e3d0871ed2b10a2fdadc4555128521ff65b7a5c
hamed <amzhamed@gmail.com>
2025-06-02 15:14:27 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -417,11 +417,11 @@ class Controller:
             for i in range(point_count):
                 if self.battery_low:
                     return
-                x = 0
-                y = y[i]
-                z = z[i]
-                print(x, y, z)
-                # self.send_position_target(x, y, z)
+                _x = 0
+                _y = y[i]
+                _z = z[i]
+                print(_x, _y, _z)
+                # self.send_position_target(_x, _y, _z)
                 time.sleep(1 / 10)
 
     def test_trajectory(self, x=0, y=0, z=0):
