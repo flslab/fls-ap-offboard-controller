d33889a24a9b551a1d317c2d4539726dfea41bf5
hamed <amzhamed@gmail.com>
2025-06-05 12:23:47 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -661,7 +661,8 @@ class Controller:
     def test_trajectory_2(self):
         waypoints = [
             [0, 0, -self.takeoff_altitude],
-            [.2, 0, -self.takeoff_altitude]
+            [-.2, 0, -self.takeoff_altitude],
+            [-.2, 0, -self.takeoff_altitude]
         ]
         trajectory = self.generate_pos_vel_path(waypoints, target_speed=1.0, dt=0.05)
 
