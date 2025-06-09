10b1d319e70e1c145b4f53afc89e1d78e0217117
hamed <amzhamed@gmail.com>
2025-06-09 10:35:15 -0700

    test trajectory

--- controller.py
+++ controller.py
@@ -853,8 +853,8 @@ class Controller:
 
         while self.running_position_estimation:
             data = shm_map[:position_size]  # Read 28 bytes
-            valid = struct.unpack("<?", data[:1])[0]  # Extract the validity flag (1 byte)
-
+            valid = struct.unpack("<?", data[3:4])[0]  # Extract the validity flag (1 byte)
+            print(valid)
             if valid:
                 x, y, z, roll, pitch, yaw = struct.unpack("<6f", data[4:28])
                 # x, y, z = struct.unpack("<3f", data[4:16])
