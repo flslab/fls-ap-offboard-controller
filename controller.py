21f2a9953b8f78e6f36c92579b495e0f0954cf74
hamed <amzhamed@gmail.com>
2025-07-18 10:11:24 -0700

    test

--- controller.py
+++ controller.py
@@ -1073,7 +1073,7 @@ class Controller:
             flight_thread = Thread(target=self.send_trajectory_from_file, args=(args.trajectory,))
         else:
             time.sleep(2)
-            flight_thread = Thread(target=self.test_trajectory_3)
+            flight_thread = Thread(target=self.test_trajectory_4())
             # flight_thread = Thread(target=self.start_mission)
             # flight_thread = Thread(target=self.test_trajectory, args=(0, 0, 0))
             # flight_thread = Thread(target=self.test_s_trajectory)
