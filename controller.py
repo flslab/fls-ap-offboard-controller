603811cb31e7dda14092f9981505019c57f4b4b9
hamed <amzhamed@gmail.com>
2025-08-05 09:29:09 -0700

    remove vel

--- controller.py
+++ controller.py
@@ -1096,13 +1096,13 @@ class Controller:
             time.sleep(1 / args.fps)
 
     def send_vicon_position(self, x, y, z, timestamp):
-        vx, vy, vz = self.velocity_estimator.update(x, y, z, timestamp=timestamp)
+        # vx, vy, vz = self.velocity_estimator.update(x, y, z, timestamp=timestamp)
         # Positive z is down, negative z is up.
         # Do not try to send positive z coordinates. Otherwise, the drone keeps ascending.
-        # self.send_position_estimate(y / 1000, x / 1000, -z / 1000)
+        self.send_position_estimate(y / 1000, x / 1000, -z / 1000)
         # self.send_velocity_estimate(vy / 1000, vx / 1000, -vz / 1000)
-        self.send_vision_odometry(y / 1000, x / 1000, -z / 1000 + 0.05, vy / 1000, vx / 1000, -vz / 1000)
-        self.send_distance_sensor(z / 10)
+        # self.send_vision_odometry(y / 1000, x / 1000, -z / 1000 + 0.05, vy / 1000, vx / 1000, -vz / 1000)
+        # self.send_distance_sensor(z / 10)
 
     def send_landing_target(self, angle_x, angle_y, distance, x=0, y=0, z=0):
         """
