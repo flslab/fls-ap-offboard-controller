083bcda3dc2d57fc6adfddfdf8afefcaf93f72c4
hamed <amzhamed@gmail.com>
2025-08-07 11:37:54 -0700

    profiling

--- vicon.py
+++ vicon.py
@@ -95,10 +95,6 @@ class ViconWrapper(threading.Thread):
                             last_time = now
                             self.logger.debug(f"Position (mm): X={pos_x:.2f}, Y={pos_y:.2f}, Z={pos_z:.2f}")
                             self.logger.debug(f"Time Interval (ms): {time_interval * 1000}")
-
-                            if time_interval < self.expected_time_interval:
-                                self.logger.debug(f"have to wait for {self.expected_time_interval - time_interval} sec")
-                                time.sleep(self.expected_time_interval - time_interval)
                         else:
                             self.logger.warning(f"\tPosition (mm): Occluded or no data")
 
