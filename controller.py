5ab715bb5248d567c78380d7d265d4ceebe21026
hamed <amzhamed@gmail.com>
2025-08-20 11:38:13 -0700

    tune

--- controller.py
+++ controller.py
@@ -902,15 +902,21 @@ class Controller:
         max_v = 5
         waypoints = [
                         [-max_v, 0, 0],
+                        [0, 0, 0],
                         [max_v, 0, 0],
+                        [0, 0, 0],
                         [max_v, 0, 0],
+                        [0, 0, 0],
                         [-max_v, 0, 0],
                     ] * 5
 
         waypoints_2 = [
                           [0, max_v, 0],
+                          [0, 0, 0],
                           [0, -max_v, 0],
+                          [0, 0, 0],
                           [0, -max_v, 0],
+                          [0, 0, 0],
                           [0, max_v, 0],
                       ] * 5
 
