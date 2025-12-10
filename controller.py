d8e72b96b6b475cf7bd7d369d4dc61d59aa349f2
hamed <amzhamed@gmail.com>
2025-12-09 16:10:00 -0800

    add servo thread

--- controller.py
+++ controller.py
@@ -1412,7 +1412,7 @@ if __name__ == "__main__":
 
     if args.idle:
         if args.servo:
-            c.servo_seq_2()
+            c.servo_seq_3()
         time.sleep(args.duration)
     else:
         if not c.arm_with_retry():
