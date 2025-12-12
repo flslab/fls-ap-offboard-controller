04975b7892fdf67ccc532ee7c584f343fb815670
hamed <amzhamed@gmail.com>
2025-12-12 11:04:42 -0800

    add range test

--- servo_pwm.py
+++ servo_pwm.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 from pi5RC import pi5RC
 
@@ -21,27 +22,54 @@ class Servo:
             del servo
         self.servos.clear()
 
+    def __len__(self):
+        return len(self.servos)
+
+    def __getitem__(self, index):
+        return self.servos[index]
+
+    def __setitem__(self, index, value):
+        self.set(index, value)
+
+
+def range_test(servos, a, b, n=5):
+    for i in range(n):
+        for j in range(len(servos)):
+            servos[i] = a
+
+        time.sleep(1)
+
+        for j in range(len(servos)):
+            servos[i] = b
+
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-n", default=2, type=int, help="number of servos [1, 4]")
+    ap.add_argument("-i", action="store_true")
+    ap.add_argument("--range-test", type=int, nargs="3", help="range test angle1 angle2 repetitions")
     args = ap.parse_args()
 
     servos = Servo(args.n)
 
     try:
-        while True:
-            user_input = input(f"Enter servo index and angle between 0 and 180:\n")
-            if user_input:
-                j, angle = user_input.strip().split(" ")
-                j, angle = int(j), int(angle)
-                if angle > 180:
-                    angle = 180
-                if angle < 0:
-                    angle = 0
+        if args.i:
+            while True:
+                user_input = input(f"Enter servo index and angle between 0 and 180:\n")
+                if user_input:
+                    j, angle = user_input.strip().split(" ")
+                    j, angle = int(j), int(angle)
+                    if angle > 180:
+                        angle = 180
+                    if angle < 0:
+                        angle = 0
 
-                if 0 <= j < args.n:
-                    servos.set(j, angle)
+                    if 0 <= j < args.n:
+                        servos.set(j, angle)
+        elif len(args.range_test):
+            range_test(servos, *args.range_test)
 
     except KeyboardInterrupt:
         print("Program stopped by user")
