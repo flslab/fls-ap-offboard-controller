e0bbbfe95267dbb77ae2fefaac219d1c164c9868
hamed <amzhamed@gmail.com>
2025-12-24 17:25:19 -0800

    add set servo cmd

--- servo_pwm.py
+++ servo_pwm.py
@@ -147,8 +147,8 @@ if __name__ == '__main__':
         elif len(args.range_test):
             range_test(servos, *args.range_test)
         elif len(args.set_all):
-            print(args.set_all)
             servos.set_all(args.set_all)
+            time.sleep(1)
 
     except KeyboardInterrupt:
         print("Program stopped by user")
